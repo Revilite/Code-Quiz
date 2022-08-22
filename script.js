6b3cd746eeacf8dcbfae19f9e4c605bcd8e4eeb4
Jacob <procoder165@gmail.com>
2022-08-22 17:58:29 -0400

    adding questions to js file

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ screenSelector(1);
 //Links to other screens
 startToTest.addEventListener("click", function(){
     screenSelector(1);
+    testing();
 })
 
 startToHs.addEventListener("click", function(){
@@ -40,3 +41,51 @@ startToHs.addEventListener("click", function(){
 
 
 
+
+
+var testing = function(){
+    //questions will be randomly generated for placement
+    //declaraction for testing phase
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include",
+        answer: "alerts",
+        wrongAnswer1: "string",
+        wrongAnswer2: "number",
+        wrongAnswer3: "booleans",
+    },
+    {
+        question: "If statements check for",
+        answer: "True Values",
+        wrongAnswer1: "False Values",
+        wrongAnswer2: "Other If statements",
+        wrongAnswer3: "Numbers",
+    }
+    
+]
+
+var questPlacement = document.querySelector(".question");
+var questionSelection = -2;
+var questionCheck = -1;
+
+//remove questions after answering 
+
+for(var i = 0; i <= questions.length; i++){
+    questionCheck = questionSelection;
+    questionSelection = Math.floor(Math.random * questions.length);
+    
+    while (questionSelection == questionCheck ){
+        questionSelection = Math.floor(Math.random * question.length);
+    }
+
+questPlacement.textContent() = "Question " + (i + 1)+ ": " + questions[questionSelection].question; 
+}
+
+
+questPlacement.textContent = "Hello World";
+}
+
+
+//Delete later
+
+testing();
